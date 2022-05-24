91f4c22cbe6de9409e7ea7cd1e9613be834bf9a4
Arun Kumar <26931@telxsids.net>
2022-05-24 12:33:00 +0530

    added a new tesing.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,37 +1,36 @@
-import axios from 'axios';
-import React, { Component } from 'react'
-import SlideLayout from '../Slider/SlideLayout'
-import './Home.css'
+import axios from "axios";
+import React, { Component } from "react";
+import SlideLayout from "../Slider/SlideLayout";
+import "./Home.css";
 import Slider from "react-slick";
-import HoriSlider from '../H-Slider/HoriSlider';
-import { Link } from 'react-router-dom';
-
-
-
+import HoriSlider from "../H-Slider/HoriSlider";
+import { Link } from "react-router-dom";
 
 export default class Home extends Component {
-    
-    
-    render() {
+  render() {
+    return (
+      <div className="body">
+        <SlideLayout />
 
-        
-       
-        return (
-            <div className='body'>
-                    <SlideLayout/>
-               
-                    <Link to='/games' style={{textDecoration:'none'}}><h3 className='left-align'>New on Liv <i class="fas fa-chevron-right"></i> </h3></Link>
-                    <HoriSlider/>
-                    <Link to='/originals' style={{textDecoration:'none'}}><h3 className='left-align'>Liv Originals<i class="fas fa-chevron-right"></i> </h3></Link>
-                    <HoriSlider/>
-                    <Link to='/movie' style={{textDecoration:'none'}}><h3 className='left-align'>Movies<i class="fas fa-chevron-right"></i> </h3></Link>
-                    <HoriSlider/>
-                    
-                
-                
-            </div>
-        )
-    }
+        <Link to="/games" style={{ textDecoration: "none" }}>
+          <h3 className="left-align">
+            New on Liv testing my git <i class="fas fa-chevron-right"></i>{" "}
+          </h3>
+        </Link>
+        <HoriSlider />
+        <Link to="/originals" style={{ textDecoration: "none" }}>
+          <h3 className="left-align">
+            Liv Originals<i class="fas fa-chevron-right"></i>{" "}
+          </h3>
+        </Link>
+        <HoriSlider />
+        <Link to="/movie" style={{ textDecoration: "none" }}>
+          <h3 className="left-align">
+            Movies<i class="fas fa-chevron-right"></i>{" "}
+          </h3>
+        </Link>
+        <HoriSlider />
+      </div>
+    );
+  }
 }
-
-
